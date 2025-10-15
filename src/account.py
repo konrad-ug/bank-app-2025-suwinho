1c32df6418402bc9da1742f8928b31da742e11f2
Mateusz <mateuszsuwinski93@gmail.com>
2025-10-15 16:00:18 +0200

    Feature 1-4

--- src/account.py
+++ src/account.py
@@ -1,5 +1,19 @@
 class Account:
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name, last_name, pesel, promo_code = None):
         self.first_name = first_name
         self.last_name = last_name
-        self.balance = 0
+        self.balance = 50.0 if is_promo_code_valid(promo_code) else 0.0
+        self.pesel = pesel if self.is_pesel_valid(pesel) else "Invalid"
+        
+
+    def is_pesel_valid(self,pesel):
+        if pesel and len(pesel) == 11:
+            return True
+        return False
+
+    def is_promo_code_valid(self, promo_code):
+        if promo_code and promo_code.startswitch("PROM_") and len(promo_code) == 8:
+            self.balance = 50.0
+        else:
+            self.balance = 0.0
+                
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,7 +3,25 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe")
+        account = Account("John", "Doe", '12345678901')
         assert account.first_name == "John"
         assert account.last_name == "Doe"
-        assert account.balance == 0
+        assert account.balance == 0.0
+        assert account.pesel == "12345678901"        
+
+    def test_pesel_to_long(self):
+        account = Account("John","Dheere","43253532153151")
+        assert account.pesel == "Invalid"
+    
+    def test_promo_valid(self):
+        account = Account("John", "Johnson","12345678901")
+        account.add_promo_code("Prom_XYZ")
+        assert account.balance == 50
+    
+    def test_promo_invalid_prefix(self):
+        account = Account("John", "Johnson","12345678901", "PRxM_Abc")
+        assert account.balance == 50
+
+    def test_promo_invalid_suffix(self):
+        account = Account("John", "Johnson","12345678901", "PRxM_Abc")
+        assert account.balance == 50
